ead54ed13cfaf4f8d687ab9296080d1a98ab6dbc
Evan Yan <evanyans@gmail.com>
2023-12-23 21:32:23 -0800

    completed front-end

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,33 +1,116 @@
 'use client'
-
 import React, {useEffect, useState} from 'react'
-import { catchAsync } from './utils'
+import { catchAsync, formatDate, myMax, getButtonColor, capFirstLetter, getColor } from './utils'
 import axios from 'axios'
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
+import { useToast } from "@/components/ui/use-toast"
+import { Card,CardContent,CardDescription, CardHeader,CardTitle,} from "@/components/ui/card"
+import Typewriter from 'typewriter-effect';
+import { addDays, format } from "date-fns"
+import { DatePickerWithRange } from '@/components/ui/datepicker'
+import { Bar, BarChart, ResponsiveContainer } from "recharts"
+import {Drawer,DrawerClose,DrawerContent,DrawerDescription,DrawerFooter,DrawerHeader,DrawerTitle,DrawerTrigger,} from "@/components/ui/drawer"
 
 export default function Home() {
   const [articles, setArticles] = useState<any>(null)
   const [inputData, setInputData] = useState('')
-
+  const [date, setDate] = useState<any>({from: addDays(new Date(), -14),to: new Date(),});
+  const { toast } = useToast()
 
   const handleSubmit = async() => {
     const fetchData = async () => {
       console.log(inputData)
-      const {data} = await axios.get(`http://127.0.0.1:8000/test?q=${inputData}`)
+      console.log(date.from)
+      console.log(date.to)
+      const {data} = await axios.get(`http://127.0.0.1:8000/test?q=${inputData}&from=${date.from}&to=${date.to}`)
       console.log(data)
       setArticles(data)
     }
-    //fetchData()
-    catchAsync(fetchData())
+    toast({
+      title: "Analyzing Articles...",
+      description: "It may take up to 20 seconds to analyze your articles on your first usage.",
+    })
+    catchAsync(fetchData)()
   }
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-      <Input value={inputData} onChange={(e) => setInputData(e.target.value)} />
-      <Button onClick={handleSubmit}>Submit</Button>
+    <div className="flex flex-col mt-10 items-center gap-2">
+      <h1 className="content-center text-6xl font-black">TINA</h1>
+      <p className="text-base font-normal	">Tactical Investment News Analyzer</p>
+      <div className="flex flex-row">
+        <p className="text-sm font-light mb-2">Try typing words like&nbsp;</p>
+        <div className="text-sm font-light">
+          <Typewriter 
+            options={{
+              strings: ['Apple', 'Tesla', 'NVIDIA', 'Netflix', 'GameStop'],
+              autoStart: true,
+              loop: true,
+            }}/>    
+        </div>
+      </div>
+      <Input className="font-light" value={inputData} onChange={(e) => setInputData(e.target.value)} placeholder="Type keywords here"/>
+      <DatePickerWithRange date={date} setDate={setDate} />
+      <Button className="mb-10 max-w-xs w-full" onClick={handleSubmit}>Analyze Articles</Button>
+      {articles  
+      && //checks if articles has been loaded yet before displaying
+      articles.map(({article, sentiment} : any, index: any) => (
+        <Card key={index} className="card">
+          <CardHeader>
+            <CardTitle>{article.source.name}</CardTitle>
+            <CardDescription>{formatDate(article.publishedAt)}</CardDescription>
+            <CardDescription className="text-blue-400"><a href={article.url} target="_blank">{article.title}</a></CardDescription>
+          </CardHeader>
+          <CardContent>
+          <Drawer>
+            <DrawerTrigger asChild>
+              <Button className={getButtonColor(sentiment.reduce(myMax).label)}>{capFirstLetter(sentiment.reduce(myMax).label)}</Button>
+            </DrawerTrigger>
+            <DrawerContent>
+              <div className="mx-auto w-full max-w-sm">
+                <DrawerHeader>
+                  <DrawerTitle>Deeper Analysis</DrawerTitle>
+                  <DrawerDescription>Compare the sentiment polarities of this article.</DrawerDescription>
+                </DrawerHeader>
+                <div className="p-4 pb-0">
+                  <div className="mt-3 h-[120px]">
+                    <ResponsiveContainer width="100%" height="100%">
+                      <BarChart data={[
+                          {
+                            score: sentiment[0].score,
+                            fill: getColor(sentiment[0].label)
+                          },
+                          {
+                            score: sentiment[1].score,
+                            fill: getColor(sentiment[1].label)
+                          },
+                          {
+                            score: sentiment[2].score,
+                            fill: getColor(sentiment[2].label)
+                          },
+                        ]}>
+                        <Bar
+                          dataKey="score"
+                          style={{
+                            fill: "fill",
+                            opacity: 0.9,
+                          }}
+                        />
+                      </BarChart>
+                    </ResponsiveContainer>
+                  </div>
+                </div>
+                <DrawerFooter>
+                  <DrawerClose asChild>
+                    <Button variant="outline">Close</Button>
+                  </DrawerClose>
+                </DrawerFooter>
+              </div>
+            </DrawerContent>
+          </Drawer>
+          </CardContent>
+        </Card>
+      ))}
     </div>
   )
 }
--- client/app/utils.tsx
+++ client/app/utils.tsx
@@ -1,8 +1,57 @@
 'use client'
 
+//
+// All the helper functions
+//
+
 export const catchAsync = (fn: any) =>
   function(...args : any[]) {
     return fn(...args).catch((err: any) => {
       console.error(err);
     });
+  };
+
+export function formatDate(isoString : any) {
+    const date = new Date(isoString);
+    return date.toLocaleDateString(undefined, { 
+      year: 'numeric', 
+      month: 'long', 
+      day: 'numeric',
+      hour: '2-digit', 
+      minute: '2-digit' 
+    });
+  }
+  
+export const myMax = (prev: any, current: any) => {
+    return (prev.score > current.score) ? prev : current
+  }
+
+export const getButtonColor = (label: string) => {
+    switch (label) {
+      case 'positive':
+        return 'bg-green-400 w-full content-center'; 
+      case 'negative':
+        return 'bg-red-400 w-full content-center';
+      case 'neutral':
+        return 'w-full content-center'; 
+      default:
+        return 'w-full content-center'; 
+    }
+  }
+
+export function capFirstLetter(str : string) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  export const getColor = (label: any) => {
+    switch (label) {
+      case 'positive':
+        return 'rgb(74 222 128)'; 
+      case 'negative':
+        return 'rgb(248 113 113)';
+      case 'neutral':
+        return 'white'; 
+      default:
+        return 'black';
+    }
   };
